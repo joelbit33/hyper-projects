64c8e7d04e3e1a08c53ae90ee3a1aef57ad05a63
joelbit33 <j.oskar.nilsson@gmail.com>
2022-12-08 00:36:27 +0100

    update q7.py

--- week_three_assignment/q7.py
+++ week_three_assignment/q7.py
@@ -1,10 +1,3 @@
-"""An online retailer provides express shipping for many of its items at a rate of $10.95
-for the first item, and $2.95 for each subsequent item. Write a function that takes the
-number of items in the order as its only parameter. Return the shipping charge for
-the order as the functions result. Include user inputs that read the number of
-items purchased from the user and display the shipping charge."""
-
-
 def calc_shipping_fee(item_amount):
     total_cost = 10.95
     for i in range(1, item_amount):
